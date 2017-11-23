b1a7606cba7c787c6995526ec32c893eeac7e427
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2017-11-23 14:42:19 +0100

    Move task procedure to doLast

--- src/main/groovy/com/github/spacialcircumstances/gradle/CreateReportFilesTask.groovy
+++ src/main/groovy/com/github/spacialcircumstances/gradle/CreateReportFilesTask.groovy
@@ -9,37 +9,37 @@ import net.masterthought.cucumber.Configuration
 
 class CreateReportFilesTask extends DefaultTask {
     String projectName
-
-    @TaskAction
-    void createReportFiles() {
-        String outputDir = project.extensions.cucumberReports.outputDir
-        String buildName = project.extensions.cucumberReports.buildName
-        Boolean parallelTesting = project.extensions.cucumberReports.parallelTesting
-        ConfigurableFileCollection reports = project.extensions.cucumberReports.reports
-        File outputDirectory = new File(outputDir)
-        if(!outputDirectory.exists()) {
-            outputDirectory.mkdirs()
-        }
-
-        Configuration config = new Configuration(outputDirectory, projectName)
-        config.setRunWithJenkins(false)
-        config.setBuildNumber(buildName)
-        config.setParallelTesting(parallelTesting)
-
-        //Check if reports exist
-        List<String> existentFiles = new ArrayList<>()
-        for(File file: reports) {
-            if(file.exists() && file.isFile()) {
-                existentFiles.add(file.path)
-            } else {
-                println "Reports file ${file.path} not found"
+    CreateReportFilesTask() {
+        doLast {
+            String outputDir = project.extensions.cucumberReports.outputDir
+            String buildName = project.extensions.cucumberReports.buildName
+            Boolean parallelTesting = project.extensions.cucumberReports.parallelTesting
+            ConfigurableFileCollection reports = project.extensions.cucumberReports.reports
+            File outputDirectory = new File(outputDir)
+            if(!outputDirectory.exists()) {
+                outputDirectory.mkdirs()
             }
-        }
 
-        ReportBuilder reportBuilder = new ReportBuilder(existentFiles, config)
-        Reportable report = reportBuilder.generateReports()
-        if(report != null) {
-            println "Failed tests:  ${report.failedSteps}"
+            Configuration config = new Configuration(outputDirectory, projectName)
+            config.setRunWithJenkins(false)
+            config.setBuildNumber(buildName)
+            config.setParallelTesting(parallelTesting)
+
+            //Check if reports exist
+            List<String> existentFiles = new ArrayList<>()
+            for(File file: reports) {
+                if(file.exists() && file.isFile()) {
+                    existentFiles.add(file.path)
+                } else {
+                    println "Reports file ${file.path} not found"
+                }
+            }
+
+            ReportBuilder reportBuilder = new ReportBuilder(existentFiles, config)
+            Reportable report = reportBuilder.generateReports()
+            if(report != null) {
+                println "Failed tests:  ${report.failedSteps}"
+            }
         }
     }
 }
