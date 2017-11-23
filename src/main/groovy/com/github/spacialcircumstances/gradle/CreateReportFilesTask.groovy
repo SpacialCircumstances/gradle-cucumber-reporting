127f8bb1e195226dd82179884d54d93ed708d345
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2017-11-23 13:23:57 +0100

    Add simple report generation

--- src/main/groovy/com/github/spacialcircumstances/gradle/CreateReportFilesTask.groovy
+++ src/main/groovy/com/github/spacialcircumstances/gradle/CreateReportFilesTask.groovy
@@ -1,8 +1,11 @@
 package com.github.spacialcircumstances.gradle
 
+import net.masterthought.cucumber.ReportBuilder
+import net.masterthought.cucumber.Reportable
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.tasks.TaskAction
+import net.masterthought.cucumber.Configuration
 
 class CreateReportFilesTask extends DefaultTask {
     File outputDir
@@ -12,6 +15,24 @@ class CreateReportFilesTask extends DefaultTask {
 
     @TaskAction
     void createReportFiles() {
-        println "Creating report file in ${outputDir.toString()} for build $buildName in $projectName"
+        if(!outputDir.exists()) {
+            outputDir.mkdirs()
+        }
+
+        Configuration config = new Configuration(outputDir, projectName)
+        config.setRunWithJenkins(false)
+        config.setBuildNumber(buildName)
+
+        //Check if reports exist
+        List<String> existentFiles = new ArrayList<>()
+        for(File file: reports) {
+            if(file.exists() && file.isFile()) {
+                existentFiles.add(file.path)
+            }
+        }
+
+        ReportBuilder reportBuilder = new ReportBuilder(existentFiles, config)
+        Reportable report = reportBuilder.generateReports()
+        println "Failed tests:  ${report.failedSteps}"
     }
 }
