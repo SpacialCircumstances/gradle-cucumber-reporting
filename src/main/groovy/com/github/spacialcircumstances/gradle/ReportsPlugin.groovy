41d5ceb9cb9814d7305d38fa933859d28a0d900f
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2018-03-05 10:20:38 +0100

    Remove unused assignment

--- src/main/groovy/com/github/spacialcircumstances/gradle/ReportsPlugin.groovy
+++ src/main/groovy/com/github/spacialcircumstances/gradle/ReportsPlugin.groovy
@@ -8,7 +8,7 @@ import org.gradle.api.tasks.testing.Test
 class ReportsPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        def extension = project.extensions.create('cucumberReports', ReportsPluginExtension)
+        project.extensions.create('cucumberReports', ReportsPluginExtension)
         Task reportTask = project.task('generateCucumberReports', type: CreateReportFilesTask) {
             description = "Creates cucumber html reports"
             group = "Cucumber reports"
